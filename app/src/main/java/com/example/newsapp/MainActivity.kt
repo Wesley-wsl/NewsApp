7360afac2b026948639d4f89825f54156c4ac1e8
Wesley-wsl <wesleywestelley@gmail.com>
2024-05-05 20:52:46 -0300

    feat: create control buttons for onBoardingScreen

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.WindowCompat
 import com.example.newsapp.presentation.onboarding.OnBoardingScreen
 import com.example.newsapp.presentation.onboarding.components.OnBoardingPage
 import com.example.newsapp.ui.theme.NewsAppTheme
@@ -20,6 +21,7 @@ import com.example.newsapp.ui.theme.NewsAppTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         installSplashScreen()
         setContent {
             NewsAppTheme {
@@ -28,21 +30,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsAppTheme {
-        Greeting("Android")
     }
 }
--- app/src/main/java/com/example/newsapp/presentation/common/NewsButton.kt
+++ app/src/main/java/com/example/newsapp/presentation/common/NewsButton.kt
@@ -1,15 +1,19 @@
 package com.example.newsapp.presentation.common
 
+import androidx.annotation.ColorRes
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.newsapp.R
 import com.example.newsapp.ui.theme.NewsAppTheme
 
 
@@ -31,6 +35,22 @@ fun NewsButton(
     }
 
 }
+
+
+@Composable()
+fun NewsTextButton(
+    text: String,
+    onCLick: () -> Unit
+) {
+    TextButton(onClick = onCLick) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.labelMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = colorResource(id = R.color.white_gray)
+        )
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable()
